19a9eb51afe327412e98b308a8f897ffaae122fe
Timur A <atomeon@gmail.com>
2016-11-29 12:00:59 +0600

    logo, intro-text smaller, clockwokr works, form steps added

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,22 @@ $(document).ready(function () {
 
     $('.basket__form, .basket__thanks').hide(); // ЧАСТИ ФОРМЫ КОТОРЫЕ БУДУТ ПОЯВЛЯТСЯ ПОЭТАПНО
 
+    $('#offerStepOne').click(function (e) { 
+        $('.basket__table').hide();
+        $('.basket__form').show();
+    });
+
+    // $('#offerStepTwo').click(function (e) { 
+    //     $('.basket__form').hide();
+    //     $('.basket__thanks').show();
+
+    //     var that = this;
+    //     setTimeout(function() {
+    //         console.log(that.currentForm);
+    //         $(that.currentForm)[0].submit();
+    //     }, 700);
+    // });
+
     $(window).scroll(function () {
         $('.basket-closed-scrolls').css("top", Math.max(0, (($(window).height() - $('.basket-closed-scrolls').outerHeight()) / 2) +
             $(window).scrollTop()) + "px");
@@ -56,13 +72,16 @@ $(document).ready(function () {
         },
 
         submitHandler: function () {
+            $('.basket__form').hide();
             $('.basket__thanks').show();
-            ga('send', 'event', 'knopka', 'lead');
+
+            // ga('send', 'event', 'knopka', 'lead');
             var that = this;
+            console.log('ASDASDASDASDSDS');
             setTimeout(function () {
                 console.log(that.currentForm);
                 $(that.currentForm)[0].submit();
-            }, 700);
+            }, 1500);
         }
     });
 });
